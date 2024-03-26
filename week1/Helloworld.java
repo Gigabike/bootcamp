318e3d2562628f8ccbe79ad27c61839bed8aac1f
Tim Ng <tim431512659@gmail.com>
2024-03-26 17:35:56 +0800

    update

---
+++ week1/Helloworld.java
@@ -0,0 +1,10 @@
+package week1;
+public class Helloworld {
+    public static void main(String[]args){
+      //Start point of the program 你好
+      //Tool 1: System.out.println()
+      System.out.println("Hello,I'm Tim. Welcome to Bootcamp. 你好");
+      System.out.println("Tim");
+    }
+
+}
